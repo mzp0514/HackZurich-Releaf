b56324bbaf0546343db083929005309d78f695ea
zhou boyan <boyan@zhous-MacBook-Pro.local>
2021-09-25 12:17:55 +0200

    re-design main page

---
+++ app/src/main/java/com/huawei/hackzurich/CameraPreview.java
@@ -0,0 +1,88 @@
+package com.huawei.hackzurich;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context) {
+        super(context);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
+
+    public CameraPreview(Context context, AttributeSet attr) {
+        super(context);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
+
+    private static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+            Log.d(TAG, "camera is not available");
+        }
+        return c;
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        mCamera = getCameraInstance();
+        setCameraDisplayOrientation((Activity) getContext(), 0, mCamera);
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mHolder.removeCallback(this);
+        mCamera.setPreviewCallback(null);
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+    }
+
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+}
--- app/src/main/java/com/huawei/hackzurich/MainActivity.kt
+++ app/src/main/java/com/huawei/hackzurich/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.huawei.hackzurich
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.os.Bundle
-import android.os.Handler
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.coroutineScope
+import com.huawei.hms.maps.HuaweiMap
+import com.huawei.hms.maps.OnMapReadyCallback
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
-import java.lang.Exception
+import androidx.core.app.ActivityCompat
 
-class MainActivity : FragmentActivity() {
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private val REQUEST_CAMERA = 0x01
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = getString(R.string.app_title)
+
+//        val cameraPreview = window.decorView.findViewById<CameraPreview>(R.id.camera_view)
+//        cameraPreview.cameraPreview
 
 //        actionBar!!.hide();
 
@@ -31,6 +40,20 @@ class MainActivity : FragmentActivity() {
 //                    getString(R.string.checking_setup_result_fail, checkException.message)
 //            }
 //        }
+
+        if (ContextCompat.checkSelfPermission(this, CAMERA)
+            !== PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    CAMERA,
+                    WRITE_EXTERNAL_STORAGE
+                ), REQUEST_CAMERA
+            )
+        }
+    }
+
+    override fun onMapReady(p0: HuaweiMap?) {
+        TODO("Not yet implemented")
     }
 
     private suspend fun checkHMS() {
