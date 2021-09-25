07833ddfac7e54f07639939382e441b6a73056d0
zhou boyan <boyan@zhous-MacBook-Pro.local>
2021-09-25 09:28:40 +0200

    add an ugly main page

--- app/src/main/java/com/huawei/hackzurich/MainActivity.kt
+++ app/src/main/java/com/huawei/hackzurich/MainActivity.kt
@@ -1,31 +1,36 @@
 package com.huawei.hackzurich
 
 import android.os.Bundle
+import android.os.Handler
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.coroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = getString(R.string.app_title)
 
-        val checkStatusTextView = findViewById<TextView>(R.id.main_check)
+//        actionBar!!.hide();
 
-        lifecycle.coroutineScope.launchWhenCreated {
-            try {
-                checkHMS()
-                checkStatusTextView.text = getString(R.string.checking_setup_result_ok)
-            } catch (checkException: Exception) {
-                checkStatusTextView.text =
-                    getString(R.string.checking_setup_result_fail, checkException.message)
-            }
-        }
+
+//        val checkStatusTextView = findViewById<TextView>(R.id.main_check)
+//
+//        lifecycle.coroutineScope.launchWhenCreated {
+//            try {
+//                checkHMS()
+//                checkStatusTextView.text = getString(R.string.checking_setup_result_ok)
+//            } catch (checkException: Exception) {
+//                checkStatusTextView.text =
+//                    getString(R.string.checking_setup_result_fail, checkException.message)
+//            }
+//        }
     }
 
     private suspend fun checkHMS() {
